32c15f4da48752a003e08e61d2d27b1ac3f5bb92
satoshiakatsuka <tsukasa7613@gmail.com>
2023-07-15 22:57:40 +0900

    add basics code
    04-07を学習

---
+++ a-tour-of-go/basics/04-functions.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func addNumber(x int, y int) int {
+	return x + y
+}
+
+func main() {
+	fmt.Println(addNumber(3, 5))
+}
---
+++ a-tour-of-go/basics/05-functions-continued.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func addNumber(x, y int) int {
+	return x + y
+}
+
+func main() {
+	fmt.Println(addNumber(14, 56))
+}
