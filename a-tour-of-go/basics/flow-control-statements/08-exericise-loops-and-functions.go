887785be7eb065dae78d4f71b41deb677a9fbb66
satoshiakatsuka <tsukasa7613@gmail.com>
2023-07-21 21:47:59 +0900

    Fix: 08-exericise-loops-and-functions.go
    add machine epsilon

--- a-tour-of-go/basics/flow-control-statements/08-exericise-loops-and-functions.go
+++ a-tour-of-go/basics/flow-control-statements/08-exericise-loops-and-functions.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func Sqrt(x float64) float64 {
+	var machine_epsilon float64 = math.Nextafter(1, 2) - 1
 	z := x / 2
 	for y := 0; y < 10; y++ {
+		prev_z := z
 		z -= (z*z - x) / (2 * z)
+		if math.Abs(z-prev_z) <= machine_epsilon {
+			break
+		}
 	}
 	return z
 }
